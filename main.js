cb95c50ab1ddc8634ca58556e84b7da402e2f5af
AparajithaTL <82936340+AparajithaTL@users.noreply.github.com>
2021-07-09 14:55:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,92 +1,107 @@
-var canvas = document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
-background_image="racing.jpg";
-console.log("background image="+background_image);
-car1_image="racecarjackie.png";
-car1_width=100;
-car1_height=90;
-car1_x=10;
-car1_y=10;
+canvas = document.getElementById("mycanvas");
+ctx = canvas.getContext("2d");
 
-car2_image="fastrackV8.png";
-car2_width=100;
-car2_height=90;
-car2_x=10;
-car2_y=110;
-
-function add()
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+greencar_width ="75";
+greencar_height="100";
+//Set initial position for a car image.
+greencar_x = "5";
+greencar_y = "225";
+function add() 
 {
-    background_imgTag = new  Image();
-    background_imgTag.onload=uploadBackground;
-    background_imgTag.src = background_image;
-    car1_imgTag = new  Image();
-    car1_imgTag.onload=uploadCar1;
-    car1_imgTag.src = car1_image;
-    car2_imgTag = new  Image();
-    car2_imgTag.onload=uploadCar2;
-    car2_imgTag.src = car2_image;
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.src = greencar_image;
 }
 
-function uploadBackground()
+function uploadBackground() 
 {
-    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+	ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
 }
 
-function uploadCar1()
+function uploadgreencar() 
 {
-    ctx.drawImage(car1_imgTag,car1_x,car1_y,car1_width,car1_height);
+	ctx.drawImage(greencar_imgTag,greencar_x,greencar_y,greencar_width,greencar_height);
 }
 
-function uploadCar2()
-{
-    ctx.drawImage(car2_imgTag,car2_x,car2_y,car2_width,car2_height);
-}
+window.addEventListener("keydown", my_keydown);
 
-window.addEventListener("keydown",my_keydown);
 function my_keydown(e)
 {
-    keyPressed=e.keyCode;
-    console.log(keyPressed);
-    if(keyPressed == '38')
-    {
-        car1_up();
-        console.log("up");
-    }
-    if(keyPressed == '40')
-    {
-        car1_down();
-        console.log("down");
-    }
-    if(keyPressed == '37')
-    {
-        car1_left();
-        console.log("left");
-    }
-    if(keyPressed == '39')
-    {
-        car1_right();
-        console.log("right");
-    }
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
 
-    if(keyPressed == '87')
-    {
-        car2_up();
-        console.log("up");
-    }
-    if(keyPressed == '83')
-    {
-        car2_down();
-        console.log("down");
-    }
-    if(keyPressed == '65')
-    {
-        car2_left();
-        console.log("left");
-    }
-    if(keyPressed == '68')
-    {
-        car2_right();
-        console.log("right");
-    }
+function up()
+{
+	if(greencar_y >= 0)
+	{
+		greencar_y = greencar_y - 10;
+		console.log("When up arrow is pressed, x = " + greencar_x + "| y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
 
+function down()
+{
+	if(greencar_y <= 300)
+	{
+		greencar_y = greencar_y + 10;
+		console.log("When down arrow is pressed, x = " + greencar_x + "| y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function left()
+{
+	if(greencar_x >= 0)
+	{
+		greencar_x = greencar_x - 10;
+		console.log("When left arrow is pressed, x = " + greencar_x + "| y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function right()
+{
+	if(greencar_x <= 700)
+	{
+		greencar_x = greencar_x + 10;
+		console.log("When right arrow is pressed, x = " + greencar_x + "| y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
